c722d66646ae67bc50f49379b3f587be7bdf22cc
JohnnyRehab <zyongsmith9090@gmail.com>
2021-02-26 07:19:48 +0900

    2021/02/26 AM 07:17 Fri

---
+++ js/ui.js
@@ -0,0 +1,31 @@
+
+/* Oscillator 1 */
+var dial = new Nexus.Dial('osc1-det',{
+  'size': [40,40],
+  'interaction': 'radial', // "radial", "vertical", or "horizontal"
+  'mode': 'relative', // "absolute" or "relative"
+  'min': 0,
+  'max': 1,
+  'step': 0,
+  'value': 0
+})
+
+var dial = new Nexus.Dial('osc1-fm',{
+  'size': [40,40],
+  'interaction': 'radial',
+  'mode': 'relative',
+  'min': 0,
+  'max': 1,
+  'step': 0,
+  'value': 0
+})
+
+var dial = new Nexus.Dial('osc1-sub',{
+  'size': [40,40],
+  'interaction': 'radial',
+  'mode': 'relative',
+  'min': 0,
+  'max': 1,
+  'step': 0,
+  'value': 0
+})
