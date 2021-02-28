2d525e92b5858ad97a5d8d27781a0b532ab4d811
JohnnyRehab <zyongsmith9090@gmail.com>
2021-02-28 17:16:23 +0900

    2020.02.28 Sun 17:15

--- js/ui.js
+++ js/ui.js
@@ -1,5 +1,5 @@
 
-/* Oscillator 1 */
+/* Osc1 Oscilators */
 var dial = new Nexus.Dial('osc1-det',{
   'size': [40,40],
   'interaction': 'radial', // "radial", "vertical", or "horizontal"
@@ -21,6 +21,57 @@ var dial = new Nexus.Dial('osc1-fm',{
 })
 
 var dial = new Nexus.Dial('osc1-sub',{
+  'size': [40,40],
+  'interaction': 'radial',
+  'mode': 'relative',
+  'min': 0,
+  'max': 1,
+  'step': 0,
+  'value': 0
+})
+
+
+/* Osc2 Oscilators */
+var button = new Nexus.Button('#osc2-ring',{
+  'size': [16,16],
+  'mode': 'toggle',
+  'state': false
+})
+
+var button = new Nexus.Button('#osc2-sync',{
+  'size': [16,16],
+  'mode': 'toggle',
+  'state': false
+})
+
+var button = new Nexus.Button('#osc2-track',{
+  'size': [16,16],
+  'mode': 'toggle',
+  'state': false
+})
+
+
+var button = new Nexus.Button('#osc2-pitch',{
+  'size': [16,16],
+  'mode': 'toggle',
+  'state': false
+})
+var dial = new Nexus.Dial('#osc2-pitch-dial',{
+  'size': [40,40],
+  'interaction': 'radial',
+  'mode': 'relative',
+  'min': 0,
+  'max': 1,
+  'step': 0,
+  'value': 0
+})
+
+var button = new Nexus.Button('#osc2-fine',{
+  'size': [16,16],
+  'mode': 'toggle',
+  'state': false
+})
+var dial = new Nexus.Dial('#osc2-fine-dial',{
   'size': [40,40],
   'interaction': 'radial',
   'mode': 'relative',
