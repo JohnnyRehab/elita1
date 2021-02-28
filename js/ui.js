51d34852f512abdee47d860b5c38ee065c54558f
JohnnyRehab <zyongsmith9090@gmail.com>
2021-03-01 06:38:03 +0900

    2021.03.01 Mon 06:37

--- js/ui.js
+++ js/ui.js
@@ -80,3 +80,39 @@ var dial = new Nexus.Dial('#osc2-fine-dial',{
   'step': 0,
   'value': 0
 })
+
+var button = new Nexus.Button('#osc2-m-env',{
+  'size': [16,16],
+  'mode': 'toggle',
+  'state': false
+})
+
+var dial = new Nexus.Dial('#osc2-a',{
+  'size': [40,40],
+  'interaction': 'radial',
+  'mode': 'relative',
+  'min': 0,
+  'max': 1,
+  'step': 0,
+  'value': 0
+})
+
+var dial = new Nexus.Dial('#osc2-d',{
+  'size': [40,40],
+  'interaction': 'radial',
+  'mode': 'relative',
+  'min': 0,
+  'max': 1,
+  'step': 0,
+  'value': 0
+})
+
+var dial = new Nexus.Dial('#osc2-amt',{
+  'size': [40,40],
+  'interaction': 'radial',
+  'mode': 'relative',
+  'min': 0,
+  'max': 1,
+  'step': 0,
+  'value': 0
+})
